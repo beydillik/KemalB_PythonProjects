5b3c83dfb16daa5eec0b8befb17ba873cde26078
Kemal Beydilli <kembeydilli@gmail.com>
2023-09-16 23:42:06 +0300

    add app description, image and links

--- main.py
+++ main.py
@@ -19,17 +19,23 @@ with col2:
     """
     st.info(content)
 
-content = "Bu bölüm deneme amaçlı yazılmıştır, tüm satırı kaplaması amaçlanmıştır."
+content = "Bu bölüm deneme amaçlı yazılmıştır, tüm satırı kaplaması amaçlanmıştır. Bu bölüm deneme amaçlı yazılmıştır, tüm satırı kaplaması amaçlanmıştır."
 st.write(content)
 
-col3, col4 = st.columns(2)
+col3, empty_col,  col4 = st.columns([1.5, 0.5, 1.5])
 
 df=pandas.read_csv("data.csv", sep=";")
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
