41a4a3588b76192aa6be927da1ed5a429f795d9c
beydillik <96373781+beydillik@users.noreply.github.com>
2023-12-07 00:53:51 +0300

    Add files via upload

---
+++ pages/4_About_Me&Project.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.subheader('This project is prepared by :orange[Kemal Beydilli] who is student at Celal Bayar University Software Engineering and also Plant Controller in Mubea Company :sunglasses:', divider='rainbow')
+st.subheader('Many thanks to :blue[Prof. Dr. Akın ÖZÇİFT] who is Machine Learning Instructer of CBU-Technologgy Faculty for his support on machine learning')
