6fd1e9b0d7b6c4448f0803292fd7b9537049d6b3
Kemal Beydilli <kembeydilli@gmail.com>
2023-12-07 23:09:30 +0300

    send email using forms

--- Home.py
+++ Home.py
@@ -1,41 +1,61 @@
 import streamlit as st
-import pandas
+import sklearn
+import numpy as np
+import pandas as pd
+import joblib
 
+def predict_rf(df):
+    rf = joblib.load('RandForest_model.sav')
+    return rf.predict(df)
+def predict_dt(df):
+    dt = joblib.load('DecisionTree_model.sav')
+    return dt.predict(df)
+def predict_svm(df):
+    svm = joblib.load('SVM_model.sav')
+    return svm.predict(df)
 
 st.set_page_config(layout="wide")
 
-col1, col2 = st.columns(2)
+st.image("images/mubea1.jpg")
 
-with col1:
-    st.image("images/KemalB.jpg", width=400)
+st.title("Electricity Consumption Forecast with Machine Learning")
+content1 = """
+Please fill the necessary information into input cells below in order to get electricity consumption forecast.
+Machine Learning Algorithms will calculate expected consumption using historical information with regression analysis.
+"""
 
-with col2:
-    st.title("Kemal Beydilli")
-    content = """
-    Experienced Plant Controller with a demonstrated history of working in the automotive industry. 
-    Reporting Professional with a Master's degree focused in Business Engineering and CPA certificate from TURMOB. 
-    Skilled in Visual Basic for Applications (VBA) for Excel, Financial Reporting, Variance Analysis, Financial Analysis, Budgeting and Cost Accounting. 
-    Started a University Software Engineering program in 2021, learning mainly object-oriented programming and expected to be graduated in 2025.
-    """
-    st.info(content)
+st.info(content1)
 
-content = "Bu bölüm deneme amaçlı yazılmıştır, tüm satırı kaplaması amaçlanmıştır. Bu bölüm deneme amaçlı yazılmıştır, tüm satırı kaplaması amaçlanmıştır."
-st.write(content)
+col1, empty_col, col2 = st.columns([3.5, 0.5, 0.5])
 
-col3, empty_col,  col4 = st.columns([1.5, 0.5, 1.5])
+production_pieces = st.number_input('Please write number of pieces to be produced:', format= '%i', value=10000, step=100)
+average_weight = st.slider('Please write expected average weight of spring:', min_value=1.0, max_value=12.0, value=2.1, step=0.1)
+cycle_time = st.slider('Please write expected cycle_time_(Pcs/min):', min_value=10.0, max_value=20.0, value=18.0, step=0.1)
+stoppage_rate = st.slider('Please write expected stoppage rate percentage :', min_value=0, max_value=100, value=25, step=1)
+scrap_rate = st.slider('Please write expected scrap rate percentage:', min_value=0.00, max_value=20.0, value=0.2, step=0.01)
+algorithm = st.radio(
+    "Select algorithm to  run",
+    ["Random Forest Regressor (recommended)", "Decision Tree Regressor", "Support Vector Machine (SVM)"],
+    captions = ["", "", ""])
+st.write("Selected Algorithm: "+ algorithm)
 
-df=pandas.read_csv("data.csv", sep=";")
+if st.button('Estimate Electricity Consumption'):
+    if algorithm == "Random Forest Regressor (recommended)":
+        electricity_consumption = predict_rf(np.array([[production_pieces,average_weight, cycle_time, stoppage_rate, scrap_rate]]))
+        st.write("Consumption Estimate is: " , ("%.2f" % electricity_consumption[0]) , "kwh")
+    elif algorithm == "Decision Tree Regressor":
+        electricity_consumption = predict_dt(np.array([[production_pieces,average_weight, cycle_time, stoppage_rate, scrap_rate]]))
+        st.write("Consumption Estimate is: " , ("%.2f" % electricity_consumption[0]) , "kwh")
+    elif algorithm == "Support Vector Machine (SVM)":
+        electricity_consumption = predict_svm(np.array([[production_pieces,average_weight, cycle_time, stoppage_rate, scrap_rate]]))
+        st.write("Consumption Estimate is: " , ("%.2f" % electricity_consumption[0]) , "kwh")
+    else:
+        st.write("Please select Algorithm first")
 
-with col3:
-    for index, row in df[:10].iterrows():
-        st.header(row["title"])
-        st.write(row["description"])
-        st.image("images/" + row["image"])
-        st.write(f"[Source Code]({row['url']})")
+    st.snow()
 
-with col4:
-    for index, row in df[10:].iterrows():
-        st.header(row["title"])
-        st.write(row["description"])
-        st.image("images/" + row["image"])
-        st.write(f"[Source Code]({row['url']})")
+"""
+  
+  
+"""
+#st.image("images/mubea2.jpg")
---
+++ pages/3_Algorithm-3_SupportVectorMachine (SVM).py
@@ -0,0 +1,27 @@
+import streamlit as st
+import nbformat
+from nbconvert import HTMLExporter
+from IPython.display import display, HTML
+
+def main():
+    # Path to your .ipynb file
+    notebook_path = r"D:\SE_Projects\MachineLearning\Mubea_EnergyConsump\pages\energy_consumption_model2_DecisionTree.ipynb"
+
+    # Read the .ipynb file
+    with open(notebook_path, "r", encoding="utf-8") as file:
+        notebook_content = file.read()
+
+    # Convert .ipynb to HTML
+    notebook = nbformat.reads(notebook_content, as_version=4)
+    html_exporter = HTMLExporter()
+    html_exporter.exclude_output_prompt = True
+    (body, resources) = html_exporter.from_notebook_node(notebook)
+    html_content = body.replace('\n', '')
+
+    # Display the .ipynb as HTML
+    st.components.v1.html(html_content, width=800, height=800, scrolling=True)
+
+if __name__ == "__main__":
+    st.set_page_config(page_title="Notebook Viewer2")
+    #st.image("images/mubea2.jpg")
+    main()
